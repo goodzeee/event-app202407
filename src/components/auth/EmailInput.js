50540618b6778db866c1b06d01da3e1b6da6a3ae
admin <qkdtmdqo14@gmail.com>
2024-07-15 09:51:09 +0900

    # 회원가입 - 비밀번호 검증 체크

--- src/components/auth/EmailInput.js
+++ src/components/auth/EmailInput.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './SignUpForm.module.scss';
 import { AUTH_URL } from '../../config/host-config';
+import { debounce } from 'lodash';
 
-const EmailInput = () => {
-
+const EmailInput = ({ onSuccess }) => {
+  
   const inputRef = useRef();
 
   // 입력한 이메일 상태
-  const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredEmail, setEnteredEmail] = useState('');
 
   // 이메일 검증 여부
   const [emailVaild, setEmailValid] = useState(false);
@@ -22,30 +23,35 @@ const EmailInput = () => {
   };
 
   // 이메일 검증 후속 처리
-  const checkEmail = async(email) => {
+  const checkEmail = debounce(async(email) => {
     if (!validateEmail(email)) {
         // 이메일이 유효하지 않다면 에러메시지 세팅
         setError('이메일 형식이 유효하지 않습니다.');
         return;
     }
 
-    // 이메일이 유효하다면 다음 단계인 중복 검사
+    // 중복 검사
     const response = await fetch(`${AUTH_URL}/check-email?email=${email}`);
     // console.log('res: ', response);
     const flag = await response.json();
-    // console.log('flag: ' , flag);
+    // console.log('flag: ', flag);
     if (flag) {
-        setEmailValid(false);
-        setError('이메일이 중복되었습니다.');
+      setEmailValid(false);
+      setError('이메일이 중복되었습니다.');
+      return;
     }
-  };
+     // 이메일 중복확인 끝
+     setEmailValid(true);
+     onSuccess(email);
+ 
+   }, 1500);
 
   const changeHandler = e => {
     const email = e.target.value;
     // const isVaild = validateEmail(email);
     // console.log('isValid', isVaild);
 
-    setEnteredEmail(email);
+    // setEnteredEmail(email);
     // setEmailValid(isVaild);
 
     // 이메일 검증 후속처리 - 이메일이 유효하지 않을 때
@@ -57,18 +63,18 @@ const EmailInput = () => {
     inputRef.current.focus();
   }, []);
 
-return (
-<>
-    <p>Step 1: 유효한 이메일을 입력해주세요.</p>
-    <input
-      ref={inputRef}
+  return (
+    <>
+      <p>Step 1: 유효한 이메일을 입력해주세요.</p>
+      <input
+        ref={inputRef}
         type="email"
         placeholder="Enter your email"
         onChange={changeHandler}
         className={!emailVaild ? styles.invalidInput : ''}
-    />
-    { !emailVaild && <p className={styles.errorMessage}>{error}</p> }
-</>
+      />
+      {!emailVaild && <p className={styles.errorMessage}>{error}</p>}
+    </>
   );
 };
 
