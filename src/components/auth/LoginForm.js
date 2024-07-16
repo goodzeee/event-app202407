d5221237e84cb0cb9383dce19c7fd8b54d47d9b3
admin <qkdtmdqo14@gmail.com>
2024-07-16 09:35:13 +0900

    # 로그인 요청 보내기 (프론트엔드 화면 렌더링 처리)

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Form, Link } from 'react-router-dom';  // Link 컴포넌트 추가
+import { Form, Link, useActionData } from 'react-router-dom';  // Link 컴포넌트 추가
 import styles from './LoginForm.module.scss';
+import { AUTH_URL } from '../../config/host-config';
 
-const LoginForm = () => (
+const LoginForm = () => {
+
+  const errorText = useActionData();
+
+  return (
   <>
     <Form method="post" className={styles.form}>
       <h1>Log in</h1>
@@ -17,11 +22,46 @@ const LoginForm = () => (
       <div className={styles.actions}>
         <button type="submit" className={styles.loginButton}>Login</button>
       </div>
+
+      {errorText && <p className={styles.error}>{errorText}</p>}
+
       <div className={styles.registerLink}>
         <Link to="/sign-up">회원이 아니십니까? 회원가입을 해보세요</Link>
       </div>
     </Form>
   </>
 );
+};
 
 export default LoginForm;
+
+
+// login form에 대한 트리거 액션 함수
+export const loginAction = async ( {request} ) => {
+
+  // 입력데이터 읽기 formData
+  const formData = await request.formData();
+  // const entries = formData.entries();
+  
+  const payload = {
+    email: formData.get('email'),
+    password: formData.get('password')
+  };
+  // console.log(payload);
+
+  const response = await fetch(`${AUTH_URL}/sign-in`, {
+    method: 'POST',
+    headers: { 'Content-Type' : 'application/json' },
+    body: JSON.stringify(payload)
+  });
+
+  // 로그인 실패한 경우
+  if (response.status === 422) {
+    const errorText = await response.text();
+    console.log(errorText);
+
+    // 액션함수가 리턴한 데이터를 컴포넌트에서 쓰는 법
+    // 📌 컴포넌트에서 useActionData 훅을 사용 !
+    return errorText;
+  }
+};
